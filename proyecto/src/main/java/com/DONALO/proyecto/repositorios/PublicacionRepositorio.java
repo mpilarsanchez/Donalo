4b83652e6a291dac54cb300e9d2ff3f2d17ccd2a
M Pilar Sanchez <sanchezmendozpilar@gmail.com>
2020-02-27 21:37:56 -0300

    Revert "Merge branch 'master' into Virgi_carrusell_"
    This reverts commit 827017612390b74f34e43bcfe26d79ebc4fa01b4.

--- proyecto/src/main/java/com/DONALO/proyecto/repositorios/PublicacionRepositorio.java
+++ proyecto/src/main/java/com/DONALO/proyecto/repositorios/PublicacionRepositorio.java
@@ -1,17 +1,11 @@
 package com.DONALO.proyecto.repositorios;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.DONALO.proyecto.entidades.Publicacion;
 
 @Repository("publicacionRepositorio")
 public interface PublicacionRepositorio extends JpaRepository<Publicacion, String> {
-	
-	@Query("SELECT c FROM Publicacion c WHERE c.titulo LIKE :q OR c.descripcion LIKE :q")
-	    public List<Publicacion> buscarPublicacion(@Param("q") String nombre);
+
 }
